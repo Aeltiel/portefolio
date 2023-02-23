f5a76b322d2895a6cc58cc6b7934a5c3feb4fb79
Ingrid Stemer <ingrid.stemer@live.fr>
2023-02-23 16:27:40 +0100

    project page finish

--- src/Component/Container.jsx
+++ src/Component/Container.jsx
@@ -1,12 +1,14 @@
-function Container({image, title, description, language}){
+function Container({image, title, description, language, id}){
+    const checkLang = language;
+    const pair = parseInt(id);
     return(
         <>
-            <div className="container__box">
+            <div className={pair%2 === 0 ? "container__box" : "Twist"}>
                 <img className="container__img" src={image} alt="screen projet booki"/>
                 <div className="container__text">
                     <h3 className="container__text--title">{title}</h3>
                     <p className="container__text--description">{description}</p>
-                    <p className="container__text--langage">{language}</p>
+                    {checkLang ? <p className="container__text--langage"><span>Langages :</span> {language}</p> : <div></div> }
                 </div>
             </div>
         </>
