6abd67d31f24601ee8c96dfae1853b23b8842f6b
Aeltiel <ingrid.stemer@live.fr>
2024-01-03 12:41:31 +0100

    animation project done

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -2,7 +2,8 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">
-        Portefolio réalisé par Ingrid Stemer en 2023 à l'aide de React. V2
+        Portefolio réalisé par Ingrid Stemer en 2023 à l'aide de React. Ceci est
+        la V2 de mon portefolio.
       </p>
     </footer>
   );
